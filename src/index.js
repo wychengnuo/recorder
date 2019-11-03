846a8bd56f7b3aa2be014c013dc3c522387d6b42
wangyan3 <wangyan3.gomefinance.com.cn>
2019-11-03 20:10:03 +0800

    h5录音初始化

---
+++ src/index.js
@@ -0,0 +1,107 @@
+import $ from 'jquery';
+import recordering from './recorder';
+import './index.css';
+window.οnlοad = function () {
+    document.documentElement.style.webkitTouchCallout = 'none';
+};
+
+$(function () {
+    var array = [];
+    var lock = false;
+    var recorder;
+    var HZRecorder = recordering(window);
+
+    // 获取音频
+    var audio = document.querySelector('audio');
+
+    // 绑定事件
+    document.getElementById('startBtn').addEventListener('touchstart', function (e) {
+        e.preventDefault();
+        if (lock) return;
+        startRecording();
+        lock = true;
+    });
+    document.getElementById('startBtn').addEventListener('touchmove', function (e) {
+        e.preventDefault();
+    });
+    document.getElementById('startBtn').addEventListener('touchend', function (e) {
+        e.preventDefault();
+        $('.animate').hide();
+        stopRecording();
+
+        // 上传音频文件
+        // recorder.upload('http://localhost:9800/test', function (data) {
+        recorder.upload('/test', function (data) {
+            playRecording(data);
+            addEvent();
+            // recorder.play2(audio, array[0]);
+        })
+    });
+
+    // 开始录音
+    function startRecording() {
+        HZRecorder.get(function (rec) {
+            // 展示录音动画
+            $('.animate').show();
+            recorder = rec;
+            recorder.start();
+        });
+    }
+
+    // 停止录音
+    function stopRecording() {
+        lock = false;
+        recorder.stop();
+    }
+
+    // 展示录音信息和回答
+    function playRecording(data) {
+        const {
+            answerAudio,
+            query,
+            answer,
+            test,
+        } = data;
+        var leftElement = '<div class="record-item left">' +
+            '<p class="record-time">query</p>' +
+            '<div class="record-content">' + query + '</div>' +
+            '</div>';
+        var rightElement = '<div class="record-item right">' +
+            '<p class="record-time flex">' +
+                'answer' +
+                '<a href="javascript:;" class="play" id="' + array.length + '_id"></a>' +
+            '</p>' +
+            '<div class="record-content">' + answer + '</div>' +
+            '</div>';
+
+        var container = document.createElement('div');
+        container.setAttribute('class', 'record-container');
+        container.innerHTML = leftElement + rightElement;
+        array.push(answerAudio);
+        document.getElementsByClassName('recording')[0].appendChild(container);
+    }
+
+    // 添加动画
+    function addAnimate() {
+        $('.animate').html(
+            '<div class="round round1"></div><div class="round round2"></div><div class="round round3"></div>'
+        );
+    }
+
+    // 点击播放
+    function addEvent() {
+        var playBtn = document.getElementsByClassName('play');
+        if (playBtn && playBtn[0]) {
+            playBtn[0].onclick = function (e) {
+                var id = e.target.getAttribute('id');
+                var index = id.substring(0, id.indexOf('_'));
+
+                var data = array[index];
+                if (data) {
+                    audio.src = window.URL.createObjectURL(data);
+                    audio.play();
+                }
+            };
+        }
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        // host: '192.168.199.224',
+        port: 9000,
+        proxy: {
+            '/test': {
+                target: 'http://192.168.199.227:9800/',
+                // target: 'http://192.168.199.227:9000/',
+                changeOrigin: true,
+                // pathRewrite: {
+                //     '^/test': 'm/v1/test'
+                // }
+            }
+        }
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [{
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1,
+                            name: '[name].[ext]',
+                            publicPath: 'images',
+                            // outputPath: 'images'
+                        }
+                    },
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            // limit: 8192,
+                            name: '[name].[ext]',
+                            publicPath: 'images',
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+            {
+                test: '/\.js$/',
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ]
+}
